1242c4df9e89f9248c5f505abbf6fa1e416a35b6
Iskandar1412 <silvapaco810@gmail.com>
2025-03-11 16:56:30 -0600

    [ADD]: Se agregó ejemplo pdf

---
+++ EjemploClases/PruebaPDF/src/main/java/iniciosecion/pruebapdf/PruebaPDF.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package iniciosecion.pruebapdf;
+
+/**
+ *
+ * @author Pacos
+ */
+public class PruebaPDF {
+
+    public static void main(String[] args) {
+        View v = new View();
+        v.setLocationRelativeTo(null);
+        v.setVisible(true);
+    }
+}
